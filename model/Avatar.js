c9823dc4b4d318051ca713d1dcf645ad5fbb025f
KehindeBello <adedotunomomeji@gmail.com>
2022-11-11 08:22:09 +0100

    modify: Avatar model

--- model/Avatar.js
+++ model/Avatar.js
@@ -15,25 +15,23 @@ const avatarSchema = new Schema({
         required: true,
         enum: ['male','female']
     },
-    accessories: [
-        {
-            hair_type: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Hairtype',
-                required: true
-            },
-            skin_color: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Skincolor',
-                required: true
-            },
-            cloth_color: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Clothcolor',
-                required: true
-            }
+    accessories:{
+        hair_type: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Hairtype',
+            required: true
+        },
+        skin_color: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Skincolor',
+            required: true
+        },
+        cloth_color: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Clothcolor',
+            required: true
         }
-    ]
+    }
 
 })
 
