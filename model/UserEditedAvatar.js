00f3be8dd9e422579d0bd459b237decc08c262e7
Adetunmbi Kehinde <52609237+kenneropia@users.noreply.github.com>
2022-11-11 21:18:54 -0800

    Update and rename created_avatar.js to UserEditedAvatar.js

---
+++ model/UserEditedAvatar.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const UserEditedAvatarSchema = new Schema({
+        name: {
+        type: String,
+        required: true
+    },
+ 
+    gender: {
+        type: String,
+        required: true,
+        enum: ['male','female']
+    },
+    selected_preset_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Avatar'
+    },
+ 
+     accessories:{
+        hair_type: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Hairtype',
+            required: true
+        },
+        skin_type: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Skincolor',
+            required: true
+        },
+        cloth_type: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Clothcolor',
+            required: true
+        }
+    }
+
+})
+
+module.exports = mongoose.model('UserEditedAvatar', UserEditedAvatarSchema);
